4653689788346a9676b178de4da81e4bf81ce220
Elmer Camargo <32989499+flapjackstan@users.noreply.github.com>
2025-09-18 09:41:24 -0700

    Update lsp.lua

--- lua/elmer_nvim/plugins/lsp.lua
+++ lua/elmer_nvim/plugins/lsp.lua
@@ -5,22 +5,22 @@ return {
     "williamboman/mason.nvim",
   },
   config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "pyright" },
-    })
-
+    local mason_lspconfig = require("mason-lspconfig")
     local lspconfig = require("lspconfig")
-    require("mason-lspconfig").setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({
-          on_attach = function(client, bufnr)
-            if client.server_capabilities.inlayHintProvider then
-              vim.lsp.inlay_hint.enable(bufnr, true)
-            end
-          end,
-        })
-      end,
+
+    mason_lspconfig.setup({
+      ensure_installed = { "pyright" },
+      handlers = {
+        function(server_name)
+          lspconfig[server_name].setup({
+            on_attach = function(client, bufnr)
+              if client.server_capabilities.inlayHintProvider then
+                vim.lsp.inlay_hint.enable(bufnr, true)
+              end
+            end,
+          })
+        end,
+      },
     })
   end,
 }
-
